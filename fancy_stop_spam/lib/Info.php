f5da1caedb6d8fbdeb70b01956caf1e39704fee4
dimkalinux <dimka.linux@gmail.com>
2012-09-02 19:29:20 +0300

    Improved FancyStopSpamInfo class

--- fancy_stop_spam/lib/Info.php
+++ fancy_stop_spam/lib/Info.php
@@ -13,14 +13,23 @@ class FancyStopSpamInfo
 
     public function __construct(array $language, array $config, $db)
     {
-        $this->language      = $language;
-        $this->config        = $config;
-        $this->db            = $db;
+        $this->language = $language;
+        $this->config   = $config;
+        $this->db       = $db;
     }
 
     public function getVersion()
     {
-        return '1.5.2';
+        $extensionId = 'fancy_stop_spam';
+        $query = array(
+            'SELECT'    => 'e.version',
+            'FROM'      => 'extensions AS e',
+            'WHERE'     => 'e.id=\''.$this->db->escape($extensionId).'\''
+        );
+        $result = $this->db->query_build($query) or error(__FILE__, __LINE__);
+        $version = $this->db->result($result);
+
+        return (empty($version) ? '' : $version);
     }
 
     public function getAuthor()
